a40fb2e9f26333590c8b3f66ce3ecb80ba5199ba
robchendev <robchendev@gmail.com>
2024-01-20 22:57:58 -0800

    action button grid spacing

--- src/components/BattleUI/ActionButton.tsx
+++ src/components/BattleUI/ActionButton.tsx
@@ -9,56 +9,68 @@ const IntermediateButton = ({ text, color }: { text: string; color: ButtonColor 
   switch (color) {
     case "pink":
       return (
-        <button className="tracking-tight w-full h-full rounded-[17px] bg-red-700 p-[3px]">
-          <div className="h-full rounded-[14px] leading-3 bg-gradient-to-b from-red-500 from-40% to-red-700 to-80%">
-            <div className="h-full w-full flex items-center pt-px justify-center rounded-[14px] bg-transparent hover:bg-red-500 transition ease-out duration-300">
-              <div className="drop-shadow-[0_2px_2px_rgba(0,0,0,.35)]">{text}</div>
-            </div>
+        <div className="rounded-[25px] font-medium text-3xl text-white text-outline bg-black w-full p-1.5 -mr-[0.35rem] -mb-[0.175rem]">
+          <div className="h-full rounded-[20px] bg-white p-[3px]">
+            <button className="tracking-tight w-full h-full rounded-[17px] bg-red-700 p-[3px]">
+              <div className="h-full rounded-[14px] leading-3 bg-gradient-to-b from-red-500 from-40% to-red-700 to-80%">
+                <div className="h-full w-full flex items-center pt-px justify-center rounded-[14px] bg-transparent hover:bg-red-500 transition ease-out duration-300">
+                  <div className="drop-shadow-[0_2px_2px_rgba(0,0,0,.35)]">{text}</div>
+                </div>
+              </div>
+            </button>
           </div>
-        </button>
+        </div>
       );
     case "orange":
       return (
-        <button className="tracking-tight w-full h-full rounded-[17px] bg-amber-600 p-[3px]">
-          <div className="h-full rounded-[14px] leading-3 bg-gradient-to-b from-amber-500 from-40% to-amber-600 to-80%">
-            <div className="h-full w-full flex items-center pt-px justify-center rounded-[14px] bg-transparent hover:bg-amber-500 transition ease-out duration-300">
-              <div className="drop-shadow-[0_2px_2px_rgba(0,0,0,.35)]">{text}</div>
-            </div>
+        <div className="rounded-[25px] font-medium text-3xl text-white text-outline bg-black w-full p-1.5 -ml-[0.35rem] -mb-[0.175rem]">
+          <div className="h-full rounded-[20px] bg-white p-[3px]">
+            <button className="tracking-tight w-full h-full rounded-[17px] bg-amber-600 p-[3px]">
+              <div className="h-full rounded-[14px] leading-3 bg-gradient-to-b from-amber-500 from-40% to-amber-600 to-80%">
+                <div className="h-full w-full flex items-center pt-px justify-center rounded-[14px] bg-transparent hover:bg-amber-500 transition ease-out duration-300">
+                  <div className="drop-shadow-[0_2px_2px_rgba(0,0,0,.35)]">{text}</div>
+                </div>
+              </div>
+            </button>
           </div>
-        </button>
+        </div>
       );
     case "green":
       return (
-        <button className="tracking-tight w-full h-full rounded-[17px] bg-green-700 p-[3px]">
-          <div className="h-full rounded-[14px] leading-3 bg-gradient-to-b from-green-500 from-40% to-green-700 to-80%">
-            <div className="h-full w-full flex items-center pt-px justify-center rounded-[14px] bg-transparent hover:bg-green-500 transition ease-out duration-300">
-              <div className="drop-shadow-[0_2px_2px_rgba(0,0,0,.35)]">{text}</div>
-            </div>
+        <div className="rounded-[25px] font-medium text-3xl text-white text-outline bg-black w-full p-1.5 -mr-[0.35rem] -mt-[0.175rem]">
+          <div className="h-full rounded-[20px] bg-white p-[3px]">
+            <button className="tracking-tight w-full h-full rounded-[17px] bg-green-700 p-[3px]">
+              <div className="h-full rounded-[14px] leading-3 bg-gradient-to-b from-green-500 from-40% to-green-700 to-80%">
+                <div className="h-full w-full flex items-center pt-px justify-center rounded-[14px] bg-transparent hover:bg-green-500 transition ease-out duration-300">
+                  <div className="drop-shadow-[0_2px_2px_rgba(0,0,0,.35)]">{text}</div>
+                </div>
+              </div>
+            </button>
           </div>
-        </button>
+        </div>
       );
     case "blue":
       return (
-        <button className="tracking-tight w-full h-full rounded-[17px] bg-sky-700 p-[3px]">
-          <div className="h-full rounded-[14px] leading-3 bg-gradient-to-b from-sky-500 from-40% to-sky-700 to-80%">
-            <div className="h-full w-full flex items-center pt-px justify-center rounded-[14px] bg-transparent hover:bg-sky-500 transition ease-out duration-300">
-              <div className="drop-shadow-[0_2px_2px_rgba(0,0,0,.35)]">{text}</div>
-            </div>
+        <div className="rounded-[25px] font-medium text-3xl text-white text-outline bg-black w-full p-1.5 -ml-[0.35rem] -mt-[0.175rem]">
+          <div className="h-full rounded-[20px] bg-white p-[3px]">
+            <button className="tracking-tight w-full h-full rounded-[17px] bg-sky-700 p-[3px]">
+              <div className="h-full rounded-[14px] leading-3 bg-gradient-to-b from-sky-500 from-40% to-sky-700 to-80%">
+                <div className="h-full w-full flex items-center pt-px justify-center rounded-[14px] bg-transparent hover:bg-sky-500 transition ease-out duration-300">
+                  <div className="drop-shadow-[0_2px_2px_rgba(0,0,0,.35)]">{text}</div>
+                </div>
+              </div>
+            </button>
           </div>
-        </button>
+        </div>
       );
   }
 };
 
 const ActionButton = ({ text, color }: { text: string; color: ButtonColor }) => {
   return (
-    <GridItem colSpan={2} className="flex justify-center">
-      <div className="rounded-[25px] font-medium text-3xl text-white text-outline bg-black w-full p-1.5">
-        <div className="h-full rounded-[20px] bg-white p-[3px]">
-          <IntermediateButton text={text} color={color} />
-        </div>
-      </div>
-    </GridItem>
+    <div className="flex justify-center h-full">
+      <IntermediateButton text={text} color={color} />
+    </div>
   );
 };
 
--- src/components/BattleUI/ActionMenu.tsx
+++ src/components/BattleUI/ActionMenu.tsx
@@ -9,19 +9,19 @@ const ActionMenu = () => {
       <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-gray-400 from-0% via-white via-50% to-gray-400 to-100% px-3 py-1.5">
         <HStack w="full" h="full">
           <ActionDialog text="What will you do?" />
-          <div className="w-full h-full">
-            <Grid
-              h="full"
-              w="full"
-              templateRows="repeat(2, 1fr)"
-              templateColumns="repeat(4, 1fr)"
-              gap={0}
-            >
+          <div className="w-full h-full -mr-1">
+            <span className="w-1/2 h-1/2 inline-block">
               <ActionButton text="Fight" color="pink" />
+            </span>
+            <span className="w-1/2 h-1/2 inline-block">
               <ActionButton text="Experience" color="orange" />
+            </span>
+            <span className="w-1/2 h-1/2 inline-block">
               <ActionButton text="Projects" color="green" />
+            </span>
+            <span className="w-1/2 h-1/2 inline-block">
               <ActionButton text="About Me" color="blue" />
-            </Grid>
+            </span>
           </div>
         </HStack>
       </div>
