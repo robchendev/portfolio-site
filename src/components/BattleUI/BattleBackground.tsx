5facd7d64d732f1278976246912ff81f8742d3ea
robchendev <robchendev@gmail.com>
2024-01-20 22:39:51 -0800

    enemy platform

---
+++ src/components/BattleUI/BattleBackground.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BattleBackground = () => {
+  return (
+    <>
+      <div className="h-1/3 w-full bg-gradient-m5 from-cyan-200 from-30% to-sky-500 to-100%" />
+      <div className="h-2/3 w-full bg-gradient-to-t from-gray-100 from-80% to-cyan-200 to-96%" />
+    </>
+  );
+};
+
+export default BattleBackground;
--- src/components/BattleUI/CombatScene.tsx
+++ src/components/BattleUI/CombatScene.tsx
@@ -1,18 +1,24 @@
 import { HStack, VStack } from "@chakra-ui/react";
 import React from "react";
 import { BattlerPanel, EnemyBattlerPanel } from "./BattlerPanel";
+import EnemyPlatform from "./EnemyPlatform";
+import BattleBackground from "./BattleBackground";
 
 const CombatScene = () => {
   return (
     <div className="h-full box-border border-b-8 border-black relative bg-cyan-200">
-      {/* Sky */}
+      {/* Sky and Background */}
       <div className="h-full w-full absolute top-0 left-0">
-        <div className="h-1/3 w-full bg-gradient-m5 from-cyan-200 from-30% to-sky-500 to-100%" />
-        <div className="h-2/3 w-full bg-gradient-to-t from-gray-100 from-80% to-cyan-200 to-96%" />
+        <BattleBackground />
       </div>
 
       {/* Vignette */}
       <div className="h-full w-full absolute top-0 left-0 bg-gradient-radial from-transparent from-60% to-[rgba(0,0,0,0.2)] to-100%" />
+
+      {/* Enemy Platform */}
+      <div className="h-full w-full absolute top-0 left-0 ">
+        <EnemyPlatform />
+      </div>
 
       {/* Pokemon Info */}
       <div className="h-full w-full absolute top-0 left-0 ">
@@ -39,7 +45,7 @@ const CombatScene = () => {
               <div className="h-full w-full bg-gray-500 py-1">
                 <div className="h-full w-full bg-gradient-to-t from-gray-400 from-0% to-gray-100 to-50% pl-3 pr-8 py-0.5">
                   <div className="skew-x-12">
-                    <BattlerPanel name="Hiring Manager" level={30} />
+                    <BattlerPanel name="Pikachu" level={30} />
                   </div>
                 </div>
               </div>
