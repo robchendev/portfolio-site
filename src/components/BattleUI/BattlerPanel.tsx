901a2d06b9ed2791c1e1acdfb5b1d4743c41ec03
robchendev <robchendev@gmail.com>
2024-01-20 23:13:28 -0800

    Ally HP Bar

--- src/components/BattleUI/BattlerPanel.tsx
+++ src/components/BattleUI/BattlerPanel.tsx
@@ -10,13 +10,35 @@ const BattlerGlimpse = ({ name, level }: { name: string; level: number }) => (
   </HStack>
 );
 
-const HitPointsBar = () => (
+const EnemyHitPointsBar = () => (
   <HStack
     className="bg-gray-700 rounded w-full h-full px-1 py-[0.3rem] drop-shadow-[2px_2px_1px_rgba(0,0,0,.15)]"
     spacing={1.5}
   >
     <div className="text-amber-400 font-extrabold tracking-wide leading-none">HP</div>
     <div className="w-full bg-amber-400 h-full"></div>
+  </HStack>
+);
+
+const HitPointsBar = ({ hpVal, maxHpVal }: { hpVal: number; maxHpVal: number }) => (
+  <HStack
+    className="bg-gray-700 rounded w-full h-full px-1 py-[0.3rem] drop-shadow-[2px_2px_1px_rgba(0,0,0,.15)]"
+    spacing={1.5}
+  >
+    <div className="text-amber-400 font-extrabold tracking-wide leading-none">HP</div>
+    <div className="h-full w-full relative">
+      <div className="h-full w-full absolute top-0 left-0">
+        <div className="w-full bg-white h-full" />
+      </div>
+      <div className="h-full w-full absolute top-0 left-0">
+        <div
+          className="bg-green-500 h-full"
+          style={{
+            width: `${(hpVal / maxHpVal) * 100}%`,
+          }}
+        />
+      </div>
+    </div>
   </HStack>
 );
 
@@ -29,25 +51,35 @@ export const EnemyBattlerPanel = ({ name, level }: { name: string; level: number
           <IoMdMale />
         </div>
         <div className="w-3/4 h-[1.25rem] pr-2">
-          <HitPointsBar />
+          <EnemyHitPointsBar />
         </div>
       </HStack>
     </div>
   );
 };
 
-export const BattlerPanel = ({ name, level }: { name: string; level: number }) => {
+export const BattlerPanel = ({
+  name,
+  level,
+  hpVal = 82,
+  maxHpVal = 100,
+}: {
+  name: string;
+  level: number;
+  hpVal?: number;
+  maxHpVal?: 100;
+}) => {
   return (
     <div className="ml-2">
       <BattlerGlimpse name={name} level={level} />
       <HStack>
         <div className="w-1/4" />
         <div className="w-3/4 h-[1.25rem] pr-2">
-          <HitPointsBar />
+          <HitPointsBar hpVal={hpVal} maxHpVal={100} />
         </div>
       </HStack>
       <div className="ml-[50%] rounded-lg bg-gray-200 mr-2 text-right px-4 font-semibold">
-        43/50
+        {hpVal}/{maxHpVal}
       </div>
     </div>
   );
