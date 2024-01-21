8eeb0a69589809f13d672de251ed7372ad8a4296
robchendev <robchendev@gmail.com>
2024-01-20 23:20:33 -0800

    HP bar thresholds and fillage

--- src/components/BattleUI/BattlerPanel.tsx
+++ src/components/BattleUI/BattlerPanel.tsx
@@ -10,39 +10,50 @@ const BattlerGlimpse = ({ name, level }: { name: string; level: number }) => (
   </HStack>
 );
 
-const EnemyHitPointsBar = () => (
-  <HStack
-    className="bg-gray-700 rounded w-full h-full px-1 py-[0.3rem] drop-shadow-[2px_2px_1px_rgba(0,0,0,.15)]"
-    spacing={1.5}
-  >
-    <div className="text-amber-400 font-extrabold tracking-wide leading-none">HP</div>
-    <div className="w-full bg-amber-400 h-full"></div>
-  </HStack>
-);
+const HitPointsBar = ({ hpVal, maxHpVal }: { hpVal: number; maxHpVal: number }) => {
+  const hpPercent = (hpVal / maxHpVal) * 100;
+  let colorTag = "bg-green-500";
+  if (hpPercent <= 25) {
+    colorTag = "bg-rose-500";
+  } else if (hpPercent <= 50) {
+    colorTag = "bg-amber-400";
+  }
 
-const HitPointsBar = ({ hpVal, maxHpVal }: { hpVal: number; maxHpVal: number }) => (
-  <HStack
-    className="bg-gray-700 rounded w-full h-full px-1 py-[0.3rem] drop-shadow-[2px_2px_1px_rgba(0,0,0,.15)]"
-    spacing={1.5}
-  >
-    <div className="text-amber-400 font-extrabold tracking-wide leading-none">HP</div>
-    <div className="h-full w-full relative">
-      <div className="h-full w-full absolute top-0 left-0">
-        <div className="w-full bg-white h-full" />
+  return (
+    <HStack
+      className="bg-gray-700 rounded w-full h-full px-1 py-[0.3rem] drop-shadow-[2px_2px_1px_rgba(0,0,0,.15)]"
+      spacing={1.5}
+    >
+      <div className="text-amber-400 font-extrabold tracking-wide leading-none">HP</div>
+      <div className="h-full w-full relative">
+        <div className="h-full w-full absolute top-0 left-0">
+          <div className="w-full bg-white h-full" />
+        </div>
+        <div className="h-full w-full absolute top-0 left-0">
+          <div
+            // Change color based on amount, use yellow and red
+            className={`h-full ${colorTag}`}
+            style={{
+              width: `${hpPercent}%`,
+            }}
+          />
+        </div>
       </div>
-      <div className="h-full w-full absolute top-0 left-0">
-        <div
-          className="bg-green-500 h-full"
-          style={{
-            width: `${(hpVal / maxHpVal) * 100}%`,
-          }}
-        />
-      </div>
-    </div>
-  </HStack>
-);
+    </HStack>
+  );
+};
 
-export const EnemyBattlerPanel = ({ name, level }: { name: string; level: number }) => {
+export const EnemyBattlerPanel = ({
+  name,
+  level,
+  hpVal = 31,
+  maxHpVal = 100,
+}: {
+  name: string;
+  level: number;
+  hpVal?: number;
+  maxHpVal?: number;
+}) => {
   return (
     <div className="mr-2">
       <BattlerGlimpse name={name} level={level} />
@@ -51,7 +62,7 @@ export const EnemyBattlerPanel = ({ name, level }: { name: string; level: number
           <IoMdMale />
         </div>
         <div className="w-3/4 h-[1.25rem] pr-2">
-          <EnemyHitPointsBar />
+          <HitPointsBar hpVal={hpVal} maxHpVal={100} />
         </div>
       </HStack>
     </div>
@@ -61,13 +72,13 @@ export const EnemyBattlerPanel = ({ name, level }: { name: string; level: number
 export const BattlerPanel = ({
   name,
   level,
-  hpVal = 82,
+  hpVal = 55,
   maxHpVal = 100,
 }: {
   name: string;
   level: number;
   hpVal?: number;
-  maxHpVal?: 100;
+  maxHpVal?: number;
 }) => {
   return (
     <div className="ml-2">
