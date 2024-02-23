4f0194b81ada725548d339ac81c5f6e6d8818d2b
robchendev <robchendev@gmail.com>
2024-02-22 16:34:42 -0800

    Projectscreen layout with colors

--- src/components/BattleUI/ProjectScreen.tsx
+++ src/components/BattleUI/ProjectScreen.tsx
@@ -1,5 +1,5 @@
 import { ProjectInfo } from "@/data/projects";
-import { HStack, Text } from "@chakra-ui/react";
+import { HStack, Text, VStack } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 
 type Subscreen = "summary" | "images" | "description";
@@ -32,29 +32,51 @@ const ProjectScreen = ({
           <HStack className="h-full">
             <div className="w-3/4 h-full bg-slate-200">
               {chosenSubscreen === "summary" && (
-                <div>
+                <div className="h-full bg-cyan-400">
                   SUMMARY<div>{chosenProject.stack?.toString()}</div>
                 </div>
               )}
               {chosenSubscreen === "images" && (
-                <div>
+                <div className="h-full bg-blue-400">
                   IMAGES<div>{chosenProject.imageUrls?.toString()}</div>
                 </div>
               )}
               {chosenSubscreen === "description" && (
-                <div>
+                <div className="h-full bg-violet-400">
                   {/* RENDER MARKDOWN? */}
                   DESCRIPTION<div>{chosenProject.description}</div>
                 </div>
               )}
             </div>
-            <div className="w-1/4 px-2 h-full bg-yellow-200 text-[3rem] leading-8 [&_button]:w-full [&_button]:text-left">
-              <Text className="text-4xl leading-8">{chosenProject.name} sadasd</Text>
-              <button onClick={() => setChosenSubscreen("summary")}>Summary</button>
-              <button onClick={() => setChosenSubscreen("images")}>Images</button>
-              <button onClick={() => setChosenSubscreen("description")}>Description</button>
-              <button onClick={() => console.log("SWITCH")}>SWITCH</button>
-              <button onClick={onExit}>Close</button>
+            <div className="w-1/4 h-full bg-yellow-200 text-[3.25rem] leading-8 ">
+              <VStack justifyContent="space-between" className="h-full">
+                <Text className="text-4xl px-2 leading-8">{chosenProject.name} sadasd</Text>
+                <div className="[&_button]:w-full [&_button]:text-left [&_button]:border-t-2 [&_button]:border-black">
+                  <button
+                    className="p-3 bg-cyan-400 "
+                    onClick={() => setChosenSubscreen("summary")}
+                  >
+                    Summary
+                  </button>
+                  <button className="p-3 bg-blue-400" onClick={() => setChosenSubscreen("images")}>
+                    Images
+                  </button>
+                  <button
+                    className="p-3 bg-violet-400 border-b-2 "
+                    onClick={() => setChosenSubscreen("description")}
+                  >
+                    Description
+                  </button>
+                </div>
+                <div className="[&_button]:w-full [&_button]:text-left [&_button]:border-t-2 [&_button]:border-black">
+                  <button className="p-2 bg-green-400" onClick={() => console.log("SWITCH")}>
+                    SWITCH
+                  </button>
+                  <button className="p-2 bg-red-400" onClick={onExit}>
+                    Close
+                  </button>
+                </div>
+              </VStack>
             </div>
           </HStack>
         </div>
