c6e5f0985777070fee77de4351147cfec11ca8d6
robchendev <robchendev@gmail.com>
2024-04-06 22:24:24 -0700

    feat: loader

--- src/components/BattleUI/ScalingWrapper.tsx
+++ src/components/BattleUI/ScalingWrapper.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import BattleUI from "./BattleUI";
 import { ActionProvider } from "@/context/ActionContext";
+import { Spinner } from "@chakra-ui/react";
 
 export const DesktopScalingWrapper = () => {
   const refOuter = useRef<HTMLDivElement>(null);
@@ -12,6 +13,7 @@ export const DesktopScalingWrapper = () => {
   const [contentWidthValue, setContentWidthValue] = useState(aspectRatioW);
   const [contentHeightValue, setContentHeightValue] = useState(aspectRatioH);
   const [scaleFactor, setScaleFactor] = useState<number | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const resizeObserver = new ResizeObserver((entries) => {
       for (const entry of entries) {
@@ -45,44 +47,61 @@ export const DesktopScalingWrapper = () => {
     if (refOuter.current) {
       resizeObserver.observe(refOuter.current);
     }
-    return () => resizeObserver.disconnect();
+    const loadTimer = setTimeout(() => {
+      setIsLoading(false);
+    }, 500); // Minimum loader display time
+    return () => {
+      resizeObserver.disconnect();
+      clearTimeout(loadTimer);
+    };
   }, []);
+
+  const Loader = () => (
+    <div className="flex items-center justify-center w-screen h-screen text-white bg-black text-4xl">
+      <Spinner thickness="5px" speed="1s" emptyColor="gray.400" color="black" size="xl" />
+    </div>
+  );
 
   return (
     <div
       ref={refOuter}
       className="flex items-center justify-center h-screen bg-black text-black overflow-hidden"
     >
-      <div
-        ref={refInner}
-        className="bg-white"
-        style={{
-          width: `${contentWidthValue}px`,
-          height: `${contentHeightValue}px`,
-        }}
-      >
+      {isLoading ? (
+        <Loader />
+      ) : (
         <div
-          className="block"
+          ref={refInner}
+          className="bg-white"
           style={{
-            width: `${aspectRatioW}px`,
-            height: `${aspectRatioH}px`,
+            width: `${contentWidthValue}px`,
+            height: `${contentHeightValue}px`,
           }}
         >
-          {scaleFactor && (
-            <div
-              style={{
-                transformOrigin: "0 0",
-                transform: `scale(${scaleFactor})`,
-              }}
-              className="w-full h-full"
-            >
-              <ActionProvider>
-                <BattleUI />
-              </ActionProvider>
-            </div>
-          )}
+          <div
+            className="block"
+            style={{
+              width: `${aspectRatioW}px`,
+              height: `${aspectRatioH}px`,
+              backgroundColor: "black",
+            }}
+          >
+            {scaleFactor && (
+              <div
+                style={{
+                  transformOrigin: "0 0",
+                  transform: `scale(${scaleFactor})`,
+                }}
+                className="w-full h-full"
+              >
+                <ActionProvider>
+                  <BattleUI />
+                </ActionProvider>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
