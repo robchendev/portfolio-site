b3797de39d726d9f3b49b8891de9b00413ceab21
robchendev <robchendev@gmail.com>
2024-05-07 16:00:41 -0700

    perf: optimize action dialog box

--- src/components/ActionMenu/ActionDialog.tsx
+++ src/components/ActionMenu/ActionDialog.tsx
@@ -8,21 +8,17 @@ const ActionDialog = ({ text }: { text: string }) => {
   const isDialogWidth = (screen === "fight" || screen === "end") && showActionMenu;
   return (
     <div
-      className={`h-full rounded-[20px] text-[2.5rem] text-gray-600 bg-black ${
+      className={`h-full p-[12px] rounded-[20px] text-[2.5rem] text-gray-600 font-medium shadow-[inset_0px_0px_0px_6px_black,inset_0px_0px_0px_9px_#fcd34d] bg-gray-500 py-[13px] px-[17px] ${
         isDialogWidth ? "w-1/2" : showActionMenu ? "w-3/4 flex-grow" : "w-3/4 flex-grow mr-2"
       } p-1.5 ml-2`}
     >
-      <div className="h-full rounded-[14px] bg-yellow-300 p-[3px]">
-        <div className="h-full rounded-[12px] bg-gray-500 py-1 px-2">
-          <div className="h-full rounded-[8px] bg-white">
-            <Text
-              noOfLines={2}
-              className="leading-[3rem] h-full w-full rounded-[9px] py-2.5 px-4 drop-shadow-[3px_2px_0px_rgba(0,0,0,.15)]"
-            >
-              {text && <TextWriter text={text} speed={10} />}
-            </Text>
-          </div>
-        </div>
+      <div className="h-full rounded-[8px] bg-white">
+        <Text
+          noOfLines={2}
+          className="leading-[3rem] h-full w-full rounded-[9px] py-2.5 px-4 drop-shadow-[3px_2px_0px_rgba(0,0,0,.15)]"
+        >
+          {text && <TextWriter text={text} speed={10} />}
+        </Text>
       </div>
     </div>
   );
