9cafcd6e996b889c09c58d79b8c5b84d1200124a
robchendev <robchendev@gmail.com>
2024-05-12 04:00:08 -0700

    style: move battler wrapper text to the right by 1

--- src/components/ScreenFight/BattlerWrapper.tsx
+++ src/components/ScreenFight/BattlerWrapper.tsx
@@ -7,7 +7,7 @@ export const BattlerWrapper = ({ children }: { children: React.ReactNode }) => {
         <div className="h-full w-full bg-white p-[0.15rem] -skew-x-12 z-0 ml-4">
           <div className="h-full w-full bg-gray-700 py-1.5 pl-1">
             <div className="h-full w-full bg-gray-500 py-1">
-              <div className="h-full w-full bg-gradient-to-t from-gray-400 from-0% to-gray-100 to-50% pl-3 pr-6 py-0.5">
+              <div className="h-full w-full bg-gradient-to-t from-gray-400 from-0% to-gray-100 to-50% pl-4 pr-6 py-0.5">
                 <div className="skew-x-12">{children}</div>
               </div>
             </div>
